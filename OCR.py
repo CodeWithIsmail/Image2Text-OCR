97d201502405a4ef8fe645e07ca93f2200061527
CodeWithIsmail <hmismail360@gmail.com>
2024-10-07 21:25:33 +0600

    Update OCR.py

--- OCR.py
+++ OCR.py
@@ -3,11 +3,10 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/extract-text', methods=['GET'])
-@app.route('/extract-img', methods=['GET'])
-
 def extract_text_api():
     return "<h1>Hello ocr</h1"
 
+@app.route('/extract-img', methods=['GET'])
 def extract_img_api():
     return "<h1>Hello img</h1"
 
